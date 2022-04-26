6d9c61b280f03e8143b94bb2cad1d75e8f4e5b16
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-04-26 08:23:11 +0200

    Ajout de la boucle pour l'affichage des 10 premiers résultats

--- src/SimpsonCard.jsx
+++ src/SimpsonCard.jsx
@@ -5,7 +5,7 @@ const SimpsonCard = ({simpson}) => {
   return (
     <div>
         <h4>{simpson.name}</h4>
-        <h6>{simpson.gender}</h6>
+        <h6>{simpson.id}</h6>
         <p><small>{simpson.normalized_name}</small></p>
         </div>
   )
--- src/SimpsonList.jsx
+++ src/SimpsonList.jsx
@@ -6,10 +6,16 @@ const SimpsonList = () => {
 return (
     <div>
         <h2>SimpsonList</h2>
-        <SimpsonCard simpson={simpsons[0]}/>
+{simpsons.map ((simpson, index) => {
+    if (index < 10)
+        return <SimpsonCard simpson={simpson} />
+        return ""
+    
+})}
+
+
     </div>
 )
 }
 
-
-export default SimpsonList
+export default SimpsonList;
